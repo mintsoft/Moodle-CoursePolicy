0ce61688fcf7084e9cb43ebfb8c09c3c32edc30c
Rob Emery <rob@mintsoft.net>
2015-07-31 20:39:34 +0100

    2.3.1 'support'

---
+++ Readme.md
@@ -0,0 +1,62 @@
+=Coursepolicy.php by Nathaniel J. Bird (April 2007) for AliveTek, Inc. www.alivetek.com.=
+
+// This is based on the policy.php 
+// If an html file named coursepolicy#.php (# symbol replaced by an actual course id), then the page will be displayed 
+//    with a requirement to click "Yes" before being able to continue with the course.
+// Modifications to lib/moodlelib.php and lang/en_utf8/moodle.php are required. 
+// A new VARCHAR field named coursepoliciesagreed is required in the mdl_user table also. 
+*/
+
+/*
+//	hacked by RE for in Moodle 2.3.1 'support'
+*/
+
+1. Add column coursepoliciesagreed VARCHAR(255) to mdl_user table
+
+This field is needed to save a string of course IDs
+
+2. Add to lang/en_utf8/moodle.php :
+
+$string['coursepolicyagree'] = 'You must agree to this policy to continue using this site.  Do you agree?';
+$string['coursepolicyagreement'] = 'Course Policy Agreement';
+$string['coursepolicyagreementclick'] = 'Click here to read the Course Policy Agreement';
+
+3. Upload the coursepolicy.php to user directory
+
+The attached zip file contains the coursepolicy.php. Upload it to moodle/user/
+
+4a. Find in lib/moodlelib.php :
+
+	if (!$USER->policyagreed and !is_siteadmin()) {
+        if (!empty($CFG->sitepolicy) and !isguestuser()) {
+            if ($preventredirect) {
+                throw new require_login_exception('Policy not agreed');
+            }
+            if ($setwantsurltome) {
+                $SESSION->wantsurl = qualified_me();
+            }
+            redirect($CFG->wwwroot .'/user/policy.php');
+        } else if (!empty($CFG->sitepolicyguest) and isguestuser()) {
+            if ($preventredirect) {
+                throw new require_login_exception('Policy not agreed');
+            }
+            if ($setwantsurltome) {
+                $SESSION->wantsurl = qualified_me();
+            }
+            redirect($CFG->wwwroot .'/user/policy.php');
+        }
+    }
+
+4b. After that section, add this code to lib/moodlelib.php :
+
+    // Check that the user has agreed to the course policy if there is one
+    if (file_exists($CFG->dataroot.'/1/coursepolicy'.$course->id.'.html')) {
+        $coursepoliciesagreed = split(',',$USER->coursepoliciesagreed);
+        // If the course id is not in coursepoliciesagreed, display the course policy
+        if(!in_array($course->id,$coursepoliciesagreed)) {
+            $SESSION->wantsurl = qualified_me();
+            redirect($coursepolicypath = $CFG->wwwroot.'/user/coursepolicy.php?id='.$course->id);
+        }
+    }
+
+If you are logged on as a test user, log OFF before trying again!
---
+++ coursepolicy.php
@@ -0,0 +1,71 @@
+<?php /* coursepolicy.php by Nathaniel J. Bird (April 2007) for AliveTek, Inc. www.alivetek.com. 
+// This is based on the policy.php 
+// If an html file named coursepolicy#.php (# symbol replaced by an actual course id), then the page will be displayed 
+//    with a requirement to click "Yes" before being able to continue with the course.
+// Modifications to lib\moodlelib.php and lang\en_utf8\moodle.php are required. 
+// A new VARCHAR field named coursepoliciesagreed is required in the mdl_user table also. 
+*/
+	
+require_once('../config.php');
+require_once($CFG->libdir.'/filelib.php');
+
+// Incoming variables
+$id = optional_param('id', 0, PARAM_INT);
+$agree = optional_param('agree', 0, PARAM_BOOL);
+$courseagreed = optional_param('courseagreed', 0, PARAM_INT);
+
+define('MESSAGE_WINDOW', true);  // This prevents the message window coming up
+
+if (!isloggedin()) {
+    require_login();
+}
+
+if ($agree and confirm_sesskey() and is_numeric($courseagreed)) {    // User has agreed
+    $USER->coursepoliciesagreed .= ','.$courseagreed;
+    if (!isguestuser()) {              // Don't remember guests
+    if (!set_field('user', 'coursepoliciesagreed', $USER->coursepoliciesagreed, 'id', $USER->id)) {
+        error('Could not save your agreement');
+    }
+}
+
+if (!empty($SESSION->wantsurl)) {
+    $wantsurl = $SESSION->wantsurl;
+    unset($SESSION->wantsurl);
+    redirect($wantsurl);
+} else {
+    redirect($CFG->wwwroot.'/');
+}
+    exit;
+}
+
+$strpolicyagree = get_string('coursepolicyagree');
+$strpolicyagreement = get_string('coursepolicyagreement');
+$strpolicyagreementclick = get_string('coursepolicyagreementclick');
+
+print_header($strpolicyagreement, $SITE->fullname, $strpolicyagreement);
+
+print_heading($strpolicyagreement);
+
+if (empty($CFG->slasharguments)) {
+    $coursepolicypage = $CFG->wwwroot.'/file.php?file=/1/coursepolicy'.$id.'.html';
+} else {
+    $coursepolicypage = $CFG->wwwroot.'/file.php/1/coursepolicy'.$id.'.html';
+}
+
+//RobHACK 2.0
+$urlContent = file_get_contents($CFG->dataroot.'/1/coursepolicy'.$id.'.html');
+$urlContent = trim($urlContent);
+echo '<div class="noticebox" align="center">';
+echo '<div style="width: 70%;" >'.$urlContent.'</div>';
+echo '</div>';
+//end robhack 2.0
+
+$linkyes    = 'coursepolicy.php'; // Send back here to store agreement in the database
+$optionsyes = array('agree'=>1, 'sesskey'=>sesskey(), 'courseagreed'=>$id);
+$linkno     = $CFG->wwwroot; // Send user to home page if they don't agree
+$optionsno  = array('sesskey'=>sesskey());
+notice_yesno($strpolicyagree, $linkyes, $linkno, $optionsyes, $optionsno);
+
+print_footer();
+
+?>
